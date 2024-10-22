042dd26c5545b6c8ba37ff493e37a3fe947436b6
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-22 11:48:59 +0800

    feat(cart):create cart

--- src/components/flowbite/navbarComponent.tsx
+++ src/components/flowbite/navbarComponent.tsx
@@ -1,4 +1,6 @@
+'use client';
 
+import Cart from "@/contents/cart/cart";
 import {
     Avatar,
     Dropdown,
@@ -11,38 +13,62 @@ import {
     NavbarLink,
     NavbarToggle,
 } from "flowbite-react";
+import { useState } from "react";
+import { CiShoppingCart } from "react-icons/ci";
 
 export default function NavbarComponent() {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+    const handleOpen = () => {
+        setIsCartOpen(true);
+    }
+
+    const handleClose = () => {
+        setIsCartOpen(false);
+    }
+
     return (
-        <Navbar fluid rounded>
-            <NavbarBrand href="https://flowbite-react.com">
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">E-COMMERCE</span>
-            </NavbarBrand>
-            <div className="flex md:order-2">
-                <Dropdown
-                    arrowIcon={false}
-                    inline
-                    label={
-                        <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-                    }
-                >
-                    <DropdownHeader>
-                        <span className="block text-sm">Bonnie Green</span>
-                        <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-                    </DropdownHeader>
-                    <DropdownItem>Settings</DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem>Sign out</DropdownItem>
-                </Dropdown>
-                <NavbarToggle />
-            </div>
-            <NavbarCollapse>
-                <NavbarLink href="#" >
-                    <span className="hover:text-red-700">Home</span>
-                </NavbarLink>
-                <NavbarLink href="#"> <span className="hover:text-red-700">Products</span></NavbarLink>
-                <NavbarLink href="#"> <span className="hover:text-red-700">History</span></NavbarLink>
-            </NavbarCollapse>
-        </Navbar>
+        <>
+            <Navbar fluid rounded>
+                <NavbarBrand href="https://flowbite-react.com">
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">E-COMMERCE</span>
+                </NavbarBrand>
+                <div className="flex md:order-2">
+                    <div className="flex justify-center items-center space-x-7">
+                        <button className="relative" type="button" onClick={handleOpen}>
+                            <CiShoppingCart className="text-3xl" />
+                            <span className="absolute top-[-5px] right-[-10px] bg-red-500 text-white text-xs rounded-full px-1">
+                                3
+                            </span>
+                        </button>
+                        <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+                            }
+                        >
+                            <DropdownHeader>
+                                <span className="block text-sm">Bonnie Green</span>
+                                <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+                            </DropdownHeader>
+                            <DropdownItem>Settings</DropdownItem>
+                            <DropdownDivider />
+                            <DropdownItem>Sign out</DropdownItem>
+                        </Dropdown>
+                    </div>
+
+                    <NavbarToggle />
+                </div>
+                <NavbarCollapse>
+                    <NavbarLink href="#">
+                        <span className="hover:text-red-700">Home</span>
+                    </NavbarLink>
+                    <NavbarLink href="#"><span className="hover:text-red-700">Products</span></NavbarLink>
+                    <NavbarLink href="#"><span className="hover:text-red-700">History</span></NavbarLink>
+                </NavbarCollapse>
+            </Navbar>
+            <Cart isOpen={isCartOpen} onClose={handleClose} />
+        </>
     );
 }
