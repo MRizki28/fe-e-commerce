a30be40fd21382175e3d452e7ed652fa4fdd04b8
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-22 16:59:50 +0800

    feat(auth): create auth handler

--- src/components/flowbite/navbarComponent.tsx
+++ src/components/flowbite/navbarComponent.tsx
@@ -13,11 +13,40 @@ import {
     NavbarLink,
     NavbarToggle,
 } from "flowbite-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
+import { jwtDecode } from 'jwt-decode';
+import Link from "next/link";
+import axios from "axios";
+import SweetAlertService from "@/helper/sweetAlert";
+
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    profile: any
+}
 
 export default function NavbarComponent() {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [isLogin, setIsLogin] = useState(true);
+    const [user, setUser] = useState<User>();
+
+    let idUser = ''
+    const checkTokenValid = () => {
+        const token: any = localStorage.getItem('token');
+        if (token) {
+            const decodeToken: any = jwtDecode(token);
+            idUser = decodeToken.id;
+            if (decodeToken.exp && decodeToken.exp < Math.floor(Date.now() / 1000)) {
+                localStorage.removeItem('token');
+                setIsLogin(false);
+            }
+        } else {
+            setIsLogin(false);
+        }
+    }
 
     const handleOpen = () => {
         setIsCartOpen(true);
@@ -27,6 +56,47 @@ export default function NavbarComponent() {
         setIsCartOpen(false);
     }
 
+    const getDataUser = async () => {
+        try {
+            const response = await axios.get('https://beecommers.up.railway.app/api/v1/user/get/' + idUser)
+            const responseData = await response.data;
+            setUser(responseData.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            SweetAlertService.logoutAlert().then(async (result) => {
+                if (result.isConfirmed) {
+                    const response = await axios.post(
+                        'https://beecommers.up.railway.app/api/v1/auth/logout', {},
+                        {
+                            headers: {
+                                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                            },
+                        }
+                    );
+                    if(response.status == 201){
+                        SweetAlertService.successLogout().then(() => {
+                            localStorage.removeItem('token');
+                            setIsLogin(false);
+                            window.location.reload();
+                        });
+                    }
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        checkTokenValid();
+        getDataUser();
+    }, [])
+
     return (
         <>
             <Navbar fluid rounded>
@@ -35,36 +105,46 @@ export default function NavbarComponent() {
                 </NavbarBrand>
                 <div className="flex md:order-2">
                     <div className="flex justify-center items-center space-x-7">
-                        <button className="relative" type="button" onClick={handleOpen}>
-                            <CiShoppingCart className="text-3xl" />
-                            <span className="absolute top-[-5px] right-[-10px] bg-red-500 text-white text-xs rounded-full px-1">
-                                3
-                            </span>
-                        </button>
-                        <Dropdown
-                            arrowIcon={false}
-                            inline
-                            label={
-                                <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-                            }
-                        >
-                            <DropdownHeader>
-                                <span className="block text-sm">Bonnie Green</span>
-                                <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-                            </DropdownHeader>
-                            <DropdownItem>Settings</DropdownItem>
-                            <DropdownDivider />
-                            <DropdownItem>Sign out</DropdownItem>
-                        </Dropdown>
+                        {!isLogin ? (
+                            <Link href="/login">Login</Link>
+                        ) : (
+                            <>
+                                <button className="relative" type="button" onClick={handleOpen}>
+                                    <CiShoppingCart className="text-3xl" />
+                                    <span className="absolute top-[-5px] right-[-10px] bg-red-500 text-white text-xs rounded-full px-1">
+                                        3
+                                    </span>
+                                </button>
+                                <Dropdown
+                                    arrowIcon={false}
+                                    inline
+                                    label={
+                                        <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+                                    }
+                                >
+                                    <DropdownHeader>
+                                        {user ? (
+                                            <>
+                                                <span className="block text-sm">{user.profile?.name}</span>
+                                                <span className="block truncate text-sm font-medium">{user.email}</span>
+                                            </>
+                                        ) : (
+                                            <span className="block text-sm">User not logged in</span>
+                                        )}
+                                    </DropdownHeader>
+                                    <DropdownItem>Settings</DropdownItem>
+                                    <DropdownDivider />
+                                    <DropdownItem onClick={handleLogout}>Logout</DropdownItem>
+                                </Dropdown>
+                            </>
+                        )}
                     </div>
-
                     <NavbarToggle />
                 </div>
                 <NavbarCollapse>
                     <NavbarLink href="#">
                         <span className="hover:text-red-700">Home</span>
                     </NavbarLink>
-                    <NavbarLink href="#"><span className="hover:text-red-700">Products</span></NavbarLink>
                     <NavbarLink href="#"><span className="hover:text-red-700">History</span></NavbarLink>
                 </NavbarCollapse>
             </Navbar>
