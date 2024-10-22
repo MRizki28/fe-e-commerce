e9be4753522cab8a4a442df1d01c7ed51ccb307f
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-22 18:28:04 +0800

    update cart

--- src/contents/detailProduct/detailProduct.tsx
+++ src/contents/detailProduct/detailProduct.tsx
@@ -1,10 +1,15 @@
 'use client'
 
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { RiMoneyDollarBoxFill } from "react-icons/ri";
-import Image from 'next/image';
+import { FaCartArrowDown } from "react-icons/fa";
 import { useRouter, usePathname } from "next/navigation";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { jwtDecode } from "jwt-decode";
+import SweetAlertService from "@/helper/sweetAlert";
 
 interface Product {
     id: string;
@@ -16,8 +21,11 @@ interface Product {
 }
 
 export default function DetailProductContent() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const [data, setData] = useState<Product | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const [isAddingToCart, setIsAddingToCart] = useState<boolean>(false);
+    const [isLogin, setIsLogin] = useState(true);
     const router = useRouter();
     const getIdFromUrl = usePathname().split("/").pop();
 
@@ -38,8 +46,48 @@ export default function DetailProductContent() {
         }
     }
 
+    const checkTokenValid = () => {
+        const token: any = localStorage.getItem('token');
+        if (token) {
+            const decodeToken: any = jwtDecode(token);
+            if (decodeToken.exp && decodeToken.exp < Math.floor(Date.now() / 1000)) {
+                localStorage.removeItem('token');
+                setIsLogin(false);
+            }
+        } else {
+            setIsLogin(false);
+        }
+    }
+
+    const addToCart = async (data: any) => {
+        setIsAddingToCart(true); 
+        try {
+            const response = await axios.post('https://beecommers.up.railway.app/api/v1/cart/add', {
+                id_product: getIdFromUrl,
+                qty: data.qty,
+            },
+                {
+                    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+                });
+            console.log(response)
+            if (response.status === 201) {
+                SweetAlertService.successAddToCart().then(() => {
+                    window.location.href = '/';
+                });
+            }
+        } catch (error: any) {
+            console.log(error);
+        } finally {
+            setIsAddingToCart(false);
+        }
+    }
+
+    const dataName = localStorage.getItem('name');
+    const dataAddress = localStorage.getItem('address');
+
     useEffect(() => {
         getDataById();
+        checkTokenValid();
     }, [getIdFromUrl]);
 
     return (
@@ -68,34 +116,49 @@ export default function DetailProductContent() {
                                 <span className="text-sm text-priceColor">{data.description}</span>
                             </div>
                             <div className="mt-2">
-                                <form >
+                                <form onSubmit={handleSubmit(addToCart)}>
                                     <div>
-                                        <input type="text" id="name" aria-label="disabled input" className="mb-5 bg-gray-100 border border-gray-300 text-sm rounded-lg   block w-full p-2.5 cursor-not-allowed dark:bg-gray-300 dark:border-[#e8e8e8] dark:placeholder-[#e8e8e8] dark:text-gray-400" defaultValue="" disabled />
+                                        <input type="text" id="name" aria-label="disabled input" className="mb-5 bg-gray-100 border border-gray-300 text-sm rounded-lg   block w-full p-2.5 cursor-not-allowed dark:bg-gray-300 dark:border-[#e8e8e8] dark:placeholder-[#e8e8e8] dark:text-gray-400" defaultValue={dataName ?? ''} disabled />
                                     </div>
                                     <div>
-                                        <input type="text" id="address" aria-label="disabled input" className="mb-5 bg-gray-100 border border-gray-300 text-sm rounded-lg   block w-full p-2.5 cursor-not-allowed dark:bg-gray-300 dark:border-[#e8e8e8] dark:placeholder-[#e8e8e8] dark:text-gray-400" defaultValue="" disabled />
+                                        <input type="text" id="address" aria-label="disabled input" className="mb-5 bg-gray-100 border border-gray-300 text-sm rounded-lg   block w-full p-2.5 cursor-not-allowed dark:bg-gray-300 dark:border-[#e8e8e8] dark:placeholder-[#e8e8e8] dark:text-gray-400" defaultValue={dataAddress ?? ''} disabled />
                                     </div>
                                     <div>
                                         <input
                                             type="number"
-                                            id="quantity"
-                                            className="mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300"
+                                            id="qty"
+                                            {...register("qty", { required: "Quantity is required" })}
+                                            className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.qty ? 'border-red-500' : 'border-gray-300'
+                                                }`}
                                             placeholder="1"
                                         />
+                                        {errors.qty && (
+                                            <p className="text-red-500 text-xs mt-1">{errors.qty?.message as string}</p>
+                                        )}
                                     </div>
 
                                     <div className="mt-5">
                                         <div>
-                                            <button type="submit" className="bg-black text-white p-2 text-sm rounded-md w-full flex justify-center hover:bg-gray-700">
-                                                +<RiMoneyDollarBoxFill className="text-xl" />
-                                            </button>
+                                            {!isLogin ? (
+                                                <Link href="/login" className="bg-black text-white p-2 text-sm rounded-md w-full flex justify-center hover:bg-gray-700">
+                                                    <RiMoneyDollarBoxFill className="text-xl" />
+                                                </Link>
+                                            ) : (
+                                                <button
+                                                    type="submit"
+                                                    className="bg-black text-white p-2 text-sm rounded-md w-full flex justify-center hover:bg-gray-700"
+                                                    disabled={isAddingToCart} // Disable button saat loading
+                                                >
+                                                    {isAddingToCart ? 'Adding...' : (<>+<FaCartArrowDown className="text-xl" /></>)} {/* Tampilkan loading */}
+                                                </button>
+                                            )}
                                         </div>
                                     </div>
                                 </form>
                             </div>
                         </div>
                         <div className="flex justify-center items-center w-full md:w-1/2 lg:w-3/5 ">
-                            <Image
+                            <img
                                 alt={data.name_product}
                                 src={`https://beecommers.up.railway.app/api/v1/product/img/${data.product_img}`}
                                 width={460}
@@ -103,7 +166,6 @@ export default function DetailProductContent() {
                                 className="w-[200px] h-[200px] md:w-[460px] md:h-[460px] object-cover"
                             />
                         </div>
-
                     </div>
                 </div>
             ) : (
