f7bfbe84b09693ff74e2d17239506f63eea86223
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-23 14:32:26 +0800

    update

--- src/contents/history/historyOrder.tsx
+++ src/contents/history/historyOrder.tsx
@@ -15,7 +15,7 @@ interface Product {
     itemTotalPrice: string;
 }
 export default function HistoryOrder() {
-       /* eslint-disable @typescript-eslint/no-explicit-any */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
     const [dataHistory, setDataHistory] = useState<Product[]>([]);
     const [loading, setLoading] = useState(false);
 
@@ -29,8 +29,8 @@ export default function HistoryOrder() {
             })
             const responseData = await response.data;
             const formattedData = responseData.data.map((item: any) => {
-                const itemTotalPrice = item.price * item.qty; 
-    
+                const itemTotalPrice = item.price * item.qty;
+
                 return {
                     ...item,
                     created_at: yearFormat(item.created_at),
@@ -41,7 +41,7 @@ export default function HistoryOrder() {
             setDataHistory(formattedData);
         } catch (error) {
             console.log(error)
-        }finally {
+        } finally {
             setLoading(false);
         }
     }
@@ -51,10 +51,10 @@ export default function HistoryOrder() {
         const year = parsedDate.getFullYear();
         const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
         const day = String(parsedDate.getDate()).padStart(2, '0');
-    
+
         return `${year}-${month}-${day}`;
     }
-    
+
 
     useEffect(() => {
         getDataHistory();
@@ -66,40 +66,41 @@ export default function HistoryOrder() {
                 <h1>History Pesanan</h1>
             </div>
             <div className="mt-5">
-            {loading ? (
-                <div className="flex justify-center items-center h-60">
-                    <span className="text-xl">Loading...</span>
-                </div>
-            ) : dataHistory.length > 0 ? (
                 <div className="grid grid-cols-1 gap-y-4 gap-2 md:grid-cols-5 mt-2">
-                    <div className="bg-white border flex flex-col max-w-xl md:max-w-md">
-                        {dataHistory.map((item, index) => (
-                            <>
-                                <Image
-                                    alt="product"
-                                    src={`https://beecommers.up.railway.app/api/v1/product/img/${item.product_img}`}
-                                    className="w-full h-60 object-cover p-3 mt-2"
-                                    width="388"
-                                    height="240"
-                                />
+                    {loading ? (
+                        <div className="flex justify-center items-center h-60">
+                            <span className="text-xl">Loading...</span>
+                        </div>
+                    ) : dataHistory.length > 0 ? (
+                        <div className="bg-white border flex flex-col max-w-xl md:max-w-md">
+                            {dataHistory.map((item, index) => (
+                                <>
+                                    <Image
+                                        alt="product"
+                                        src={`https://beecommers.up.railway.app/api/v1/product/img/${item.product_img}`}
+                                        className="w-full h-60 object-cover p-3 mt-2"
+                                        width="388"
+                                        height="240"
+                                    />
 
-                                <div key={index} className="p-5 flex flex-col flex-grow border-t-2">
-                                    <div className="flex items-center">
-                                        <span className="text-semiBlack">{item.name_product}</span>
-                                        <span className="ml-auto text-[12px]">{item.created_at}</span>
+                                    <div key={index} className="p-5 flex flex-col flex-grow border-t-2">
+                                        <div className="flex items-center">
+                                            <span className="text-semiBlack">{item.name_product}</span>
+                                            <span className="ml-auto text-[12px]">{item.created_at}</span>
+                                        </div>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-brownSkincare">Rp. {item.price}</h5>
+                                        <p className="mb-3 font-normal text-[12px] text-black">Total {item.qty} Produk: <b>Rp. {item.itemTotalPrice}</b></p>
                                     </div>
-                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-brownSkincare">Rp. {item.price}</h5>
-                                    <p className="mb-3 font-normal text-[12px] text-black">Total {item.qty} Produk: <b>Rp. {item.itemTotalPrice}</b></p>
-                                </div>
-                            </>
-                        ))}
-                    </div>
+                                </>
+                            ))}
+                        </div>
+
+                    ) : (
+                        <div className="flex justify-center items-center h-60">
+                            <span className="text-xl">No data</span>
+                        </div>
+                    )}
                 </div>
-            ) : (
-                <div className="flex justify-center items-center h-60">
-                    <span className="text-xl">No data</span>
-                </div>
-            )}
             </div>
         </div >
     )
